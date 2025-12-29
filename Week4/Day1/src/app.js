a3d1fdffa8d3a814ce12ac6f598227e842960c30
Mahak Saini <mahaks.hestabit@gmail.com>
2025-12-29 18:33:43 +0530

    Week 4 Task 1

---
+++ Week4/Day1/src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const config = require('./config');
+const loadApp = require('./loaders/app'); 
+const logger = require('./utils/logger');
+
+async function startServer() {
+  const app = express();
+
+  await loadApp(app);
+
+  app.listen(config.port, () => {
+    logger.info(`Server started on port ${config.port}`);
+  });
+}
+
+startServer();
